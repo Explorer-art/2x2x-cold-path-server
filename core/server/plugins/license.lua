377b95fd72d4233956107d59d92cbcfce3db3c28
Truzme_ <56657421+Explorer-art@users.noreply.github.com>
2024-08-01 23:06:41 +0300

    Update license.lua

--- core/server/plugins/license.lua
+++ core/server/plugins/license.lua
@@ -4,7 +4,7 @@ local M = {}
 local api
 
 local function license(client, args)
-	api.call_function("chat_message", "<color=yellow>=========================</color>\nДанный сервер использует ядро сервера 2x2x, разработанное Truzme_. Ядро находится в свободном доступе, вы можете делать с ним всё что хотите пока указываете изначального автора ядра (Truzme_).\nGitHub: https://github.com/Explorer-art/2x2x-cold-path-server\n<color=yellow>=========================</color>", "message", client, true)
+	api.call_function("chat_message", "<color=yellow>=========================</color>\nДанный сервер использует ядро сервера 2x2x, разработанное Truzme_. Ядро находится в свободном доступе, вы можете делать с ним всё что хотите пока указываете изначального автора ядра (Truzme_).\nGitHub: https://github.com/Explorer-art/2x2x-cold-path-server\n<color=yellow>=========================</color>", "system", client, true)
 end
 
 function M.init(_api)
